046cda49dcaefd88ed449aca3b90ec71ff03895d
Anna Zinkovich <63413735+Anna-Zinkovich@users.noreply.github.com>
2022-10-24 15:40:03 +0200

    Update sql_marketing_attribution.sql

--- sql_marketing_attribution.sql
+++ sql_marketing_attribution.sql
@@ -1,4 +1,4 @@
-/* CoolTShirts is a fictional wedsite that sells shirts of all kinds. 
+/* CoolTShirts is a fictional website that sells shirts of all kinds. 
 Recently, CTS started a few marketing campaigns to increase website visits and purchases. 
 Using touch attribution, they’d like to map their customers’ journey: from initial visit to purchase. 
 They can use that information to optimize their marketing campaigns and I as a Data Analyst will help them in that.
